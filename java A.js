de80536dbffb63d755ea82991c14b901f60fd6fe
AhmadSOD <ahmadwasel06@gmail.com>
2023-12-18 12:51:53 +0100

    a

--- java A.js
+++ java A.js
@@ -5,3 +5,21 @@ function openPopup(topic) {
 function closePopup(topic) {
     document.getElementById("popup" + topic.charAt(0).toUpperCase() + topic.slice(1)).style.display = "none";
 }
+function toonVerberg(elementId) {
+    // Verberg alle tekstgedeelten
+    verbergAlleTeksten();
+
+    // Toon of verberg het geselecteerde tekstgedeelte
+    var tekstElement = document.getElementById(elementId);
+    tekstElement.style.display = (tekstElement.style.display === "none") ? "block" : "none";
+}
+
+function verbergAlleTeksten() {
+    // Verberg alle tekstgedeelten
+    for (var i = 1; i <= 6; i++) {
+        var huidigTekstElement = document.getElementById('tekst' + i);
+        if (huidigTekstElement) {
+            huidigTekstElement.style.display = "none";
+        }
+    }
+}
